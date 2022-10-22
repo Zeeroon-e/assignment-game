d11324520f4a77a5b67aece8267dd6c94c346b8e
Zeeroon-e <chia.kmohamad@hotmail.com>
2022-10-22 18:05:24 +0200

    små pill scss

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -8,14 +8,14 @@ function Search() {
     let data: any = JSON.parse(localStorage.getItem('games') || '[]')
     
     
-    function toggledBtn(event) {
+    function toggledBtn(event: any) {
       if (event.target.checked) {
 
         console.log('button is checked');
         setSearchByResult('won')
 
       } else {
-        
+
         console.log('button is unchecked');
         setSearchByResult('lost')
       }
@@ -23,46 +23,47 @@ function Search() {
     }
     console.log(searchByResult);
     
-    function searchBtn (){
+    // function searchBtn (){
     
 
       
-      /* data.filter((val) => {
-        if (searchedValue == '') {
-          return val
-        }
-        else if (val.fname.includes(searchedValue) || val.sname.includes(searchedValue) ) {
-          return val
-        }
-      }).map(item => {
-        console.log(item);
-      }) */
-      // let searchedArray = data.find(({fname, sname}) => fname || sname == searchedValue)
+    //   /* data.filter((val) => {
+    //     if (searchedValue == '') {
+    //       return val
+    //     }
+    //     else if (val.fname.includes(searchedValue) || val.sname.includes(searchedValue) ) {
+    //       return val
+    //     }
+    //   }).map(item => {
+    //     console.log(item);
+    //   }) */
+    //   // let searchedArray = data.find(({fname, sname}) => fname || sname == searchedValue)
  
-      // console.log(data);
+    //   // console.log(data);
       
-    }
+    // }
     
     
   return (
     <div className='search-container'>
       <section className='search-section'>
-        <input type="text"  onChange={(e) => setSearchedValue(e.target.value)}/>
-
+        <input type="text" placeholder='Search for player name' onChange={(e) => setSearchedValue(e.target.value)}/>
+        <p> u must search name first and then <br /> toggle between the colors! <br /> 🟩 = won <br /> 🟥 = lost </p>
         <label className='switch'>
           <input type="checkbox" value={searchByResult} onClick={toggledBtn}/>
           <span className='slider round'></span>
         </label>
         
-        <button className='src-btn' onClick={searchBtn}>Search</button>
+        
         
 
       </section>
       <section className='display-section'>
-        {data.filter((val) => {
+        {data.filter((val: any) => {
         if (searchedValue == '') {
           return val
-        }
+        } 
+        
         else if (val.fname.includes(searchedValue) || val.sname.includes(searchedValue) ) {
           
           if (val.fname.includes(searchedValue) && val.fresult.includes(searchByResult)) {
@@ -73,7 +74,7 @@ function Search() {
             return val
             
           }
-          return val
+          
         }
       }).map(item => (
         <p> Date Played <br /> {item.date} <br />{item.fname} vs {item.sname} <br /> {item.fgoals} | {item.sgoals} <br /> {item.fresult} | {item.sresult} </p>
